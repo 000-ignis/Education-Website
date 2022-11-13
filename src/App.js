37ba289e0c95cad75d1605503a3680710ee98f0c
Tarry Gaoseb <godfrey@mindsinaction.com.na>
2022-11-13 11:16:33 +0200

    log:completed initilisation of the courses page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/common/header/Header";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import About from './components/about/About';
+import CourseHome from './components/allcourses/CourseHome';
 
 
 function App() {
@@ -14,8 +15,8 @@ function App() {
       <Switch>
         <Route path='/' exact component={Home} />
         <Route path='/about' exact component={About} />
-        {/* <Route exact path='/courses' component={CourseHome} />
-        <Route exact path='/team' component={Team} />
+        <Route path='/courses' exact component={CourseHome} />
+        {/* <Route exact path='/team' component={Team} />
         <Route exact path='/pricing' component={Pricing} />
         <Route exact path='/jouranl' component={Blog} />
         <Route exact path='/contact' component={Contact} /> */}
